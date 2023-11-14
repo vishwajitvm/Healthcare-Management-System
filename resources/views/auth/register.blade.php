e4c57ef092e9a35d5de4f82de154c264ca0863eb
vishwajitvm <vishwajitmall50@gmail.com>
2023-11-14 05:31:16 +0530

    changes for signup or register -HSM day 2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,223 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <x-validation-errors class="mb-4" />
+<head>
+    <title>HMS | Register</title>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description"
+        content="Admindek Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
+    <meta name="keywords"
+        content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
+    <meta name="author" content="colorlib" />
 
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+    <link rel="icon" href="{{asset('backend/files/assets/images/favicon.ico')}}" type="image/x-icon">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/files/bower_components/bootstrap/css/bootstrap.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('backend/files/assets/pages/waves/css/waves.min.css')}}" type="text/css" media="all">
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/files/assets/icon/feather/css/feather.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/files/assets/icon/themify-icons/themify-icons.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/files/assets/icon/icofont/css/icofont.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/files/assets/icon/font-awesome/css/font-awesome.min.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/files/assets/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/files/assets/css/pages.css')}}">
+</head>
+
+<body themebg-pattern="theme1">
+
+    <div class="theme-loader">
+        <div class="loader-track">
+            <div class="preloader-wrapper">
+                <div class="spinner-layer spinner-blue">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+                <div class="spinner-layer spinner-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+                <div class="spinner-layer spinner-yellow">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+                <div class="spinner-layer spinner-green">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
+    <section class="login-block">
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
 
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
-
-                            <div class="ms-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
-                                ]) !!}
+                    <form class="md-float-material form-material" method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="text-center">
+                            <img src="{{asset('backend/files/assets/images/logo.png')}}" alt="logo.png">
+                        </div>
+                        <div class="auth-box card">
+                            <div class="card-block">
+                                <div class="row m-b-20">
+                                    <div class="col-md-12">
+                                        <h3 class="text-center txt-primary">Sign up</h3>
+                                    </div>
+                                </div>
+                                <div class="row m-b-20">
+                                    <div class="col-md-6">
+                                        <a href="#!"
+                                            class="btn btn-facebook m-b-20 btn-block waves-effect waves-light"><i
+                                                class="icofont icofont-social-facebook"></i>facebook</a>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <a href="#!" class="btn btn-twitter m-b-0 btn-block waves-effect waves-light"><i
+                                                class="icofont icofont-social-twitter"></i>twitter</a>
+                                    </div>
+                                </div>
+                                <p class="text-muted text-center p-b-5">Sign up with your regular account</p>
+                                <div class="form-group form-primary">
+                                    <input type="text"  class="form-control" name="name" :value="old('name')" required autofocus autocomplete="name">
+                                    <span class="form-bar"></span>
+                                    <label class="float-label">Choose Username</label>
+                                </div>
+                                <div class="form-group form-primary">
+                                    <input type="text" class="form-control" name="email" :value="old('email')" required autocomplete="username">
+                                    <span class="form-bar"></span>
+                                    <label class="float-label">Your Email Address</label>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group form-primary">
+                                            <input type="password" class="form-control" name="password" required autocomplete="new-password">
+                                            <span class="form-bar"></span>
+                                            <label class="float-label">Password</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group form-primary">
+                                            <input type="password" class="form-control"
+                                            name="password_confirmation" required autocomplete="new-password">
+                                            <span class="form-bar"></span>
+                                            <label class="float-label">Confirm Password</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row m-t-25 text-left">
+                                    <div class="col-md-12">
+                                        <div class="checkbox-fade fade-in-primary">
+                                            <label>
+                                                <input type="checkbox" value>
+                                                <span class="cr"><i
+                                                        class="cr-icon icofont icofont-ui-check txt-primary"></i></span>
+                                                <span class="text-inverse">I read and accept <a href="'.route('terms.show').'">Terms &amp;
+                                                        Conditions.</a></span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="checkbox-fade fade-in-primary">
+                                            <label>
+                                                <input type="checkbox" value>
+                                                <span class="cr"><i
+                                                        class="cr-icon icofont icofont-ui-check txt-primary"></i></span>
+                                                <span class="text-inverse"><a href="'.route('policy.show').">Privacy Policy</a>
+                                                    </span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row m-t-30">
+                                    <div class="col-md-12">
+                                        <button
+                                            class="btn btn-primary btn-md btn-block waves-effect text-center m-b-20">Sign
+                                            up now</button>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <p class="text-inverse text-left m-b-0">Thank you.</p>
+                                        <p class="text-inverse text-left"><a href="{{ route('login') }}"><b>Back to
+                                                    Login</b></a></p>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <img src="{{asset('backend/files/assets/images/auth/Logo-small-bottom.png')}}"
+                                            alt="small-logo.png">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </x-label>
+                    </form>
+
                 </div>
-            @endif
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ms-4">
-                    {{ __('Register') }}
-                </x-button>
             </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+
+        </div>
+
+    </section>
+
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/jquery/js/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/jquery-ui/js/jquery-ui.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/popper.js/js/popper.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/bootstrap/js/bootstrap.min.js')}}"></script>
+
+    <script src="{{asset('backend/files/assets/pages/waves/js/waves.min.js')}}"></script>
+
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js')}}"></script>
+
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/modernizr/js/modernizr.js')}}"></script>
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/modernizr/js/css-scrollbars.js')}}"></script>
+
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/i18next/js/i18next.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/i18next-xhr-backend/js/i18nextXHRBackend.min.js')}}">
+    </script>
+    <script type="text/javascript"
+        src="{{asset('backend/files/bower_components/i18next-browser-languagedetector/js/i18nextBrowserLanguageDetector.min.js')}}">
+    </script>
+    <script type="text/javascript" src="{{asset('backend/files/bower_components/jquery-i18next/js/jquery-i18next.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('backend/files/assets/js/common-pages.js')}}"></script>
+</body>
+
+</html>
